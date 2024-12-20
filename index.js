a89b23c9ecacaf7d88a0a87b381584ae255edb8e
Salino3 <flaviobcn012@gmail.com>
2024-12-20 10:27:16 +0100

    start server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
