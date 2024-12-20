41b614b82fdd37a4b3f32fbc31264e58043f5128
Salino3 <flaviobcn012@gmail.com>
2024-12-20 10:43:54 +0100

    connected

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
+const { db } = require("./db");
 
 dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+db.connect((err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Connected to the MySQL database!");
+  }
+});
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
