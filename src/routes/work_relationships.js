796b0a71f24081ba9d0c9a06e61024b2b45981a2
Salino3 <flaviobcn012@gmail.com>
2024-12-26 10:47:51 +0100

    routerWorkRelationships done

--- src/routes/work_relationships.js
+++ src/routes/work_relationships.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const workRelationshipsController = require("../controllers/work_relationships");
+const { verifyJWT } = require("../middleware/verify-token");
 
 const routerWorkRelationships = express.Router();
 
 routerWorkRelationships.post(
   "/pending/:userId",
+  verifyJWT("userId"),
   workRelationshipsController.pendingRelationship
 );
 
 routerWorkRelationships.patch(
   "/active/:companyId",
+  verifyJWT("companyId"),
   workRelationshipsController.activeRelationship
 );
 
 routerWorkRelationships.delete(
   "/left/:userId/:companyId",
+  verifyJWT(""),
   workRelationshipsController.leftRelationship
 );
 
